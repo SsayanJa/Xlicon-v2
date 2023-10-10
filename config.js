3c14502766f7ad8d6b204fa08c64f2aa9943d7db
SsayanJa <145949770+SsayanJa@users.noreply.github.com>
2023-10-10 18:23:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801975492880","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801975492880","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801975492880"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
+global.Owner = ["919339361276","919339361276"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["919339361276","919339361276"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["919339361276"];
+global.OwnerName = "😈 SAYAN 😈";
+global.BotName = "ROBO 2.1";
 global.packname = "X-2.0 Bot";                             //Do not change.
 global.author = "By: Slasher";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
